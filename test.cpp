1564d603fe60efdeb96a9e7fb097cb037074e2a6
y0u-di <j1815@matsue-ct.ac.jp>
2021-11-10 17:37:49 +0900

    ソート追加&完成

--- test.cpp
+++ test.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 int sum(int []);
-double standard_deviation(int []);
-int deviation(int,double,double);
+double sta_dev(int []);
+int dev(int,double,double);
+int compar(const void*,const void*);
 
 int main(){
     int i;
@@ -14,17 +16,32 @@ int main(){
 
     s_ave = sum(science)/20.0;
     e_ave = sum(english)/20.0;
-    s_sta_dev = standard_deviation(science);
-    e_sta_dev = standard_deviation(english);
+    s_sta_dev = sta_dev(science);
+    e_sta_dev = sta_dev(english);
 
     printf("理科\n");
     printf("合計点：%d,平均点：%.2f,標準偏差：%.2f\n",sum(science),s_ave,s_sta_dev);
     printf("英語\n");
     printf("合計点：%d,平均点：%.2f,標準偏差：%.2f\n",sum(english),e_ave,e_sta_dev);
 
+    printf("偏差値\n");
     for(i=0;i<20;i++){
-	printf("学生%2d：理科=>%d，英語=>%d\n",i+1,deviation(science[i],s_ave,s_sta_dev),deviation(english[i],e_ave,e_sta_dev));
+	printf("学生%2d：理科=>%d，英語=>%d\n",i+1,dev(science[i],s_ave,s_sta_dev),dev(english[i],e_ave,e_sta_dev));
     }
+
+    qsort(science,20,sizeof(int),compar);
+    qsort(english,20,sizeof(int),compar);
+
+    printf("理科\n");
+    for(i=0;i<20;i++){
+	printf("%d ",science[i]);
+    }
+    printf("\n");
+    printf("英語\n");
+    for(i=0;i<20;i++){
+	printf("%d ",english[i]);
+    }
+    printf("\n");
 }
 
 int sum(int arr[]){
@@ -36,7 +53,7 @@ int sum(int arr[]){
     return sum;
 }
 
-double standard_deviation(int arr[]){
+double sta_dev(int arr[]){
     int i;
     double ave = sum(arr) / 20.0;
     double var = 0;
@@ -46,6 +63,14 @@ double standard_deviation(int arr[]){
     return sqrt(var/20);
 }
 
-int deviation(int score,double ave,double sta_dev){
+int dev(int score,double ave,double sta_dev){
     return (score - ave) / sta_dev * 10 + 50;
+}
+
+int compar(const void* a, const void* b) {
+    if (*(int*)a < *(int*)b) {
+        return 1;
+    } else {
+        return -1;
+    }
 }
